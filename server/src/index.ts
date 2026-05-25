993ede1732e3bea8a9a53c283f88bcdfe6b9a732
ankitRawat99 <rawat.ankitt520@gmail.com>
2026-05-25 19:37:01 +0530

    latest

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:5173",
   "https://scribble-clone-alpha.vercel.app",
-];
+]; 
 
 const io = new Server(server, {
   cors: {
